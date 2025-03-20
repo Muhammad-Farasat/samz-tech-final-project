6ba09542868be1f0db85ab7d65a94651c00641fb
Muhammad-Farasat <farasatkhan687@gmail.com>
2025-03-21 00:52:38 +0500

    Home page completed also responsive

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const menu = document.getElementById('menu')
+const smallScreenNav = document.getElementById('smallScreenNav')
 
 menu.addEventListener('click', () => {
     menu.classList.toggle('active')
+    smallScreenNav.classList.toggle('active')
 })
 
 const headings = document.querySelectorAll(' #support p, #services p, #about p, #parthner p ')
